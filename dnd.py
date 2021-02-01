5200e9dee0d201e76ab75ff1cccfd2927d43b63e
noahgill409 <noahgill409@gmail.com>
2021-02-01 14:43:57 -0600

    object orientation
    Cleaned up various methods inline with OOP, implemented the basis for Weapon, Die, and Dice(Die) class. This is an older project so I'm just committing for the sake of archival.

--- dnd.py
+++ dnd.py
@@ -1,12 +1,35 @@
 from bedevere import *
 from bedevere.discrete_sums import *
 from bedevere.matrix import *
+import re
 
 advantage = 'advantage'
 disadvantage = 'disadvantage'
 trivantage = 'trivantage'
 
 conditions = [advantage, disadvantage, trivantage]
+roll_generic_pattern = re.compile(r'(\d*)d(4|6|8|10|12)', re.I)
+whitespace_pattern = re.compile(r'\s')
+
+
+def dice_string_parser(dice_string: str):
+	"""Parse a dice string into one or more tuples of information for die and dice construction
+
+	Separate types of dice separated by any number of spaces and a + or -."""
+
+	dice_type_tuples = whitespace_pattern.sub('', dice_string.strip()).split('+')
+
+	dice_list = []
+
+	for dtt in dice_type_tuples:
+		n, sides = roll_generic_pattern.groups()
+		n = int(n) if n else 1
+		sides = int(sides)
+
+		for i in range(n):
+			dice_list.append(Die(sides))
+
+	dice = Dice(dice_list)
 
 
 class Die:
@@ -23,8 +46,19 @@ class Die:
 	def average_roll(self, n: int = 1) -> float:
 		return np.average(self.rolls, weights=self.weights) * n
 
+	def min_roll(self, n: int = 1) -> int:
+		return n * min(self.rolls)
+
 	def max_roll(self, n: int = 1) -> int:
 		return n * max(self.rolls)
+
+	def to_dict(self) -> dict:
+		d = {}
+
+		for roll, weight in zip(self.rolls, self.weights):
+			d[roll] = weight
+
+		return d
 
 
 class WeightedDie(Die):
@@ -59,46 +93,63 @@ class GreatWeaponFightingDie(Die):
 		return np.average(self.rolls, weights=mean_weights) * n
 
 
-class Weapon:
+class Dice(Die):
 
-	def __init__(self, attack_bonus: int, damage_bonus: int, damage_dice: Union[Die, List[Die]]):
-		self.attack_bonus = attack_bonus
-		self.damage_bonus = damage_bonus
+	def __init__(self, dice: Union[Die, List[Die]]):
+		super().__init__(1)
 
-		if type(damage_dice) == list:
-			self.damage_dice = damage_dice
+		if isinstance(dice, Die):
+			self.dice = [dice]
 
 		else:
-			self.damage_dice = [damage_dice]
+			self.dice = dice
 
-	def attack(self, condition: str = None, extra_damage_dice: Union[Die, List[Die]] = None, extra_damage_bonus: int = 0) \
+		minimum_roll = sum(map(lambda die: die.min_roll(), self.dice))
+		maximum_roll = sum(map(lambda die: die.max_roll(), self.dice))
+		list_of_distributions = list(map(lambda die: die.to_dict(), self.dice))
+		roll_distribution = convolution_list(list_of_distributions)
+
+		assert minimum_roll == min(roll_distribution.keys())
+		assert maximum_roll == max(roll_distribution.keys())
+
+		self.rolls = np.arange(minimum_roll, maximum_roll + 1)
+		self.weights = np.asarray(list(map(lambda r: roll_distribution[r], self.rolls)))
+
+		assert is_stochastic(self.weights)
+
+	def roll(self, n: int = 1) -> list:
+		unflattened_rolls = list(map(lambda d: d.roll(n), self.dice))
+		return [r for roll in unflattened_rolls for r in roll]
+
+
+class Weapon:
+
+	def __init__(self, name: str, attack_bonus: int, damage_bonus: int, damage_dice: Die):
+		self.name = name
+		self.attack_bonus = attack_bonus
+		self.damage_bonus = damage_bonus
+		self.damage_dice = damage_dice
+
+	def attack(self, condition: str = None, extra_damage_dice: Die = None, extra_damage_bonus: int = 0) \
 			-> Tuple[int, int, object]:
-
-		if type(extra_damage_dice) == Die:
-			extra_damage_dice = [extra_damage_dice]
-
-		elif extra_damage_dice is None:
-			extra_damage_dice = []
 
 		main_damage = 0
 		extra_damage = 0
+		is_Critical_Hit = False
+		C = None
 
 		try:
 			roll_to_hit = roll_d20(self.attack_bonus, condition)
 
-		except CriticalHit as CH:
-			main_damage = roll_dice(self.damage_dice, self.damage_bonus, is_critical=True)
-			extra_damage = roll_dice(extra_damage_dice, extra_damage_bonus, is_critical=True)
-			return main_damage, extra_damage, CH
+		except CriticalHit as C:
+			is_Critical_Hit = True
 
-		except CriticalFumble as CF:
-			main_damage = roll_dice(self.damage_dice, self.damage_bonus)
-			extra_damage = roll_dice(extra_damage_dice, extra_damage_bonus)
-			return main_damage, extra_damage, CF
+		except CriticalFumble as C:
+			pass
 
-		main_damage = roll_dice(self.damage_dice, self.damage_bonus)
-		extra_damage = roll_dice(extra_damage_dice, extra_damage_bonus)
-		return main_damage, extra_damage, None
+		main_damage = roll_dice(self.damage_dice, self.damage_bonus, is_Critical_Hit)
+		extra_damage = roll_dice(extra_damage_dice, extra_damage_bonus, is_Critical_Hit)
+		return main_damage, extra_damage, C
 
 
 D4 = Die(4)
@@ -114,21 +165,9 @@ def roll_generic(die: Die, n: int = 1, bonus: int = 0) -> int:
 	return sum(die.roll(n)) + bonus
 
 
-def roll_dice(dice: Union[Die, List[Die]], bonus: int = 0, is_critical: bool = False) -> int:
-
-	if type(dice) == Die:
-		dice = [Die]
-
+def roll_dice(dice: Die, bonus: int = 0, is_critical: bool = False) -> int:
 	dice_multiplier = 2 if is_critical else 1
-	dice_rolls = []
-
-	for die in dice:
-		die_roll = die.roll(dice_multiplier)
-
-		for dr in die_roll:
-			dice_rolls.append(dr)
-
-	return sum(dice_rolls) + bonus
+	return sum(dice.roll(dice_multiplier)) + bonus
 
 
 def roll_d20(bonus: int = None, condition: str = None) -> int:
@@ -167,7 +206,7 @@ def test():
 	weapon = Weapon(5, 4, [D6, D6])
 
 	for condition in conditions:
-		main_damage, extra_damage, Crit = weapon.attack(condition, extra_damage_dice=[D6, D8, D8, D8])
+		main_damage, extra_damage, Crit = weapon.attack(condition, extra_damage_dice=Dice([D6, D8, D8, D8]))
 
 		if type(Crit) == CriticalHit:
 			print('crit', main_damage, extra_damage)
